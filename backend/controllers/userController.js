4bfab6a03f84e628d68b06e6d68b8da00d9ff96d
NayaabAkhtar <nayaabakhtar154@gmail.com>
2024-07-19 22:34:18 +0530

    minor changes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -64,7 +64,7 @@ export const login = async (req, res) => {
 
         const token = await jwt.sign(tokenData, process.env.JWT_SECRET_KEY, { expiresIn: '3d' });
 
-        return res.status(200).cookie("token", token, { maxAge: 3 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'strict', secure: process.env.NODE_ENV !== "development", }).json({
+        return res.status(200).cookie("token", token, { maxAge: 3 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'strict',}).json({
             _id: user._id,
             username: user.username,
             fullName: user.fullName,
