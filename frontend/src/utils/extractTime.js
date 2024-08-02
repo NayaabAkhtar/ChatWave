deee02737d824d8059f396a0ff4a983b060e7e42
NayaabAkhtar <nayaabakhtar154@gmail.com>
2024-08-02 13:59:54 +0530

    changes in timing

--- frontend/src/utils/extractTime.js
+++ frontend/src/utils/extractTime.js
@@ -1,6 +1,6 @@
 export function extractTime(dateString) {
 	const date = new Date(dateString);
-	const hours = padZero(date.getHours())%12;
+	const hours = padZero(date.getHours());
 	const minutes = padZero(date.getMinutes());
 	return `${hours}:${minutes}`;
 }
