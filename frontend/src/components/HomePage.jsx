cf3f7ebbb33b5e8871ee27984dc4bb3b3a6ff6a7
NayaabAkhtar <nayaabakhtar154@gmail.com>
2024-08-10 13:08:08 +0530

    scrollbar

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -15,7 +15,7 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div className='flex h-[500px] md:h-[570px] rounded-lg overflow-hidden bg-gray-700 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-0'>
+    <div className='flex h-[570px] rounded-lg overflow-hidden bg-gray-700 bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-0'>
       <Sidebar/>
       <MessageContainer/>
     </div>
